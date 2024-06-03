9fc4c2ee948c711e4719cb741d797d5294e67d45
ibra2407 <mehloldex@gmail.com>
2024-06-03 14:40:23 +0800

    deleted useless config file

--- db.js
+++ db.js
@@ -1,4 +1,14 @@
-import {db, Table} from './db.config.js'
+import AWS from 'aws-sdk'
+
+AWS.config.update({
+    region: "ap-southeast-1",
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+})
+
+const db = new AWS.DynamoDB.DocumentClient()
+
+const Table = 'users'
 
 // Create or Update users
 const createOrUpdate = async (data = {}) =>{
